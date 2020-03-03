39115021096a0da895e59f79894294703d28a12c
John Louis Rumingan <jmrumingan@gmail.com>
2020-03-03 14:33:56 -0500

    Changed shares/show to show company,purchase_date, and owner JSON

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -10,6 +10,16 @@ class SharesController < ApplicationController
         user = User.all.find_by(id: params[:id])
         
         shares_owned_by_user = Share.all.select{|share| share.user = user}
-        render json: shares_owned_by_user
+
+        array_copy = []
+        shares_owned_by_user.each do |share|
+            share_copy = {}
+            share_copy["company"] = share.company.symbol
+            share_copy["purchase_date"] = share.purchase_date
+            share_copy["owner"] = share.user.name
+            array_copy << share_copy
+        end 
+
+        render json: array_copy
     end 
 end
