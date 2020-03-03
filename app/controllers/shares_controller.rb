7964fd7b70f637b0b2a92abf42390af537821091
John Louis Rumingan <jmrumingan@gmail.com>
2020-03-03 18:40:32 -0500

    Added ability to buy shares from the profile page. Need to do so from marketplace.

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,5 +1,8 @@
 class SharesController < ApplicationController
 
+    #only to be used in development 
+    skip_before_action :verify_authenticity_token
+
     def index 
         shares = Share.all
         render json: shares
@@ -9,18 +12,46 @@ class SharesController < ApplicationController
     def show 
         user = User.all.find_by(id: params[:id])
         
-        shares_owned_by_user = Share.all.select{|share| share.user = user}
-
+        shares_owned_by_user = Share.all.select{|share| share.user == user}
         array_copy = []
         shares_owned_by_user.each do |share|
             share_copy = {}
+            share_copy["id"] = share.id
             share_copy["symbol"] = share.company.symbol
             share_copy["purchase_date"] = share.purchase_date
             share_copy["owner"] = share.user.name
             share_copy["company_id"] = share.company.id
+            share_copy["user_id"] = share.user.id
             array_copy << share_copy
         end 
 
         render json: array_copy
     end 
+
+
+    def create
+
+        user = User.all.find_by(id: params[:user_id])
+        company = Company.all.find_by(id: params[:company_id])
+        
+
+        date_purchased = Time.now.to_s[0...10]
+        
+        new_share = Share.new
+        new_share.company = company
+        new_share.user = user 
+        new_share.purchase_date = date_purchased
+
+        new_share.save
+        # byebug
+
+        share_copy = {}
+        share_copy["id"] = new_share.id
+        share_copy["symbol"] = new_share.company.symbol
+        share_copy["purchase_date"] = new_share.purchase_date
+        share_copy["owner"] = new_share.user.name
+        share_copy["company_id"] = new_share.company.id
+        share_copy["user_id"] = new_share.user.id
+        render json: share_copy
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'prices/show'
   get 'users/index'
   get 'users/show'
+  post '/shares', to: 'shares#create'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
